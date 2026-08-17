a0024756e3b35b3ea57e78f86ab580676fb6ae94
frndo-ebo <fernando.bessa0301@gmail.com>
2026-08-17 15:15:39 -0300

    Add product database for fruits, vegetables, and herbs
    Added a database of products including fruits, vegetables, and herbs with their respective prices and images.

--- produtos.js
+++ produtos.js
@@ -1,1 +1,345 @@
 
+// produtos.js - Banco de dados de produtos Verde e Grão
+export const produtos = [
+  // ========== FRUTAS (15) ==========
+  {
+    id: 1,
+    nome: 'Limão Siciliano',
+    preco: 'R$12,50/kg',
+    imagem: 'imagens_frutas/limao.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 2,
+    nome: 'Banana-prata',
+    preco: 'R$7,49/kg',
+    imagem: 'imagens_frutas/banana.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 3,
+    nome: 'Maçã',
+    preco: 'R$12,50uni',
+    imagem: 'imagens_frutas/maca.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 4,
+    nome: 'Uva Verde sem semente',
+    preco: 'R$15,00(500g)',
+    imagem: 'imagens_frutas/uva.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 5,
+    nome: 'Melão',
+    preco: 'R$10,00uni',
+    imagem: 'imagens_frutas/melao.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 6,
+    nome: 'Pera Portuguesa',
+    preco: 'R$6,00uni',
+    imagem: 'imagens_frutas/pera.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 7,
+    nome: 'Maracujá',
+    preco: 'R$4,50uni',
+    imagem: 'imagens_frutas/maracuja.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 8,
+    nome: 'Morango',
+    preco: 'R$8,50(bandeja)',
+    imagem: 'imagens_frutas/morango.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 9,
+    nome: 'Mamão Papaya',
+    preco: 'R$5,00uni',
+    imagem: 'imagens_frutas/mamao.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 10,
+    nome: 'Abacaxi Pérola',
+    preco: 'R$8,00uni',
+    imagem: 'imagens_frutas/abacaxi.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 11,
+    nome: 'Manga Palmer',
+    preco: 'R$6,50Kg',
+    imagem: 'imagens_frutas/manga.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 12,
+    nome: 'Laranja Pêra',
+    preco: 'R$5,99Kg',
+    imagem: 'imagens_frutas/laranja.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 13,
+    nome: 'Melancia',
+    preco: 'R$18,00uni',
+    imagem: 'imagens_frutas/melancia.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 14,
+    nome: 'Kiwi',
+    preco: 'R$14,00Kg',
+    imagem: 'imagens_frutas/kiwi.jpg',
+    categoria: 'frutas'
+  },
+  {
+    id: 15,
+    nome: 'Coco',
+    preco: 'R$6,00uni',
+    imagem: 'imagens_frutas/coco.jpg',
+    categoria: 'frutas'
+  },
+
+  // ========== LEGUMES (18) ==========
+  {
+    id: 16,
+    nome: 'Tomate cereja',
+    preco: 'R$8,90caixa',
+    imagem: 'https://cdn.pixabay.com/photo/2019/07/11/10/14/cherry-tomato-4330441_1280.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 17,
+    nome: 'Cenoura',
+    preco: 'R$8,99/kg',
+    imagem: 'imagens_legumes/cenoura.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 18,
+    nome: 'Pepino',
+    preco: 'R$2,00uni',
+    imagem: 'imagens_legumes/pepino.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 19,
+    nome: 'Alface',
+    preco: 'R$2,00uni',
+    imagem: 'imagens_legumes/alface.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 20,
+    nome: 'Tomate',
+    preco: 'R$5,50/kg',
+    imagem: 'imagens_legumes/tomate.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 21,
+    nome: 'Batata',
+    preco: 'R$4,50/kg',
+    imagem: 'imagens_legumes/batata.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 22,
+    nome: 'Abobrinha',
+    preco: 'R$3,50/kg',
+    imagem: 'imagens_legumes/abobrinha.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 23,
+    nome: 'Brócolis',
+    preco: 'R$6,00/uni',
+    imagem: 'imagens_legumes/brocolis.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 24,
+    nome: 'Cebola',
+    preco: 'R$3,80/kg',
+    imagem: 'imagens_legumes/cebola.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 25,
+    nome: 'Pimentão',
+    preco: 'R$5,00/kg',
+    imagem: 'imagens_legumes/pimentao.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 26,
+    nome: 'Chuchu',
+    preco: 'R$2,50/kg',
+    imagem: 'imagens_legumes/chuchu.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 27,
+    nome: 'Berinjela',
+    preco: 'R$4,20/kg',
+    imagem: 'imagens_legumes/berinjela.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 28,
+    nome: 'Couve-Flor',
+    preco: 'R$6,50/uni',
+    imagem: 'imagens_legumes/couve.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 29,
+    nome: 'Beterraba',
+    preco: 'R$3,90/kg',
+    imagem: 'imagens_legumes/beterraba.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 30,
+    nome: 'Alho',
+    preco: 'R$2,50/ctb',
+    imagem: 'imagens_legumes/alho.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 31,
+    nome: 'Vagem',
+    preco: 'R$5,80/kg',
+    imagem: 'imagens_legumes/vagem.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 32,
+    nome: 'Mandioquinha',
+    preco: 'R$8,90/kg',
+    imagem: 'imagens_legumes/mandioquinha.jpg',
+    categoria: 'legumes'
+  },
+  {
+    id: 33,
+    nome: 'Aspargo',
+    preco: 'R$2,24/Kg',
+    imagem: 'imagens_legumes/aspargo.jpg',
+    categoria: 'legumes'
+  },
+
+  // ========== VERDURAS E TEMPEROS (15) ==========
+  {
+    id: 34,
+    nome: 'Cebolinha',
+    preco: 'R$12,90/kg',
+    imagem: 'imagens_temperos/cebolinha.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 35,
+    nome: 'Coentro',
+    preco: 'R$2,50/maço',
+    imagem: 'imagens_temperos/coentro.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 36,
+    nome: 'Salsinha',
+    preco: 'R$2,50/maço',
+    imagem: 'imagens_temperos/salsinha.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 37,
+    nome: 'Manjericão',
+    preco: 'R$3,00/maço',
+    imagem: 'imagens_temperos/manjericao.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 38,
+    nome: 'Alecrim',
+    preco: 'R$4,00/maço',
+    imagem: 'imagens_temperos/alecrim.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 39,
+    nome: 'Hortelã',
+    preco: 'R$3,00/maço',
+    imagem: 'imagens_temperos/hortela.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 40,
+    nome: 'Orégano',
+    preco: 'R$4,50/maço',
+    imagem: 'imagens_temperos/oregano.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 41,
+    nome: 'Tomilho',
+    preco: 'R$4,50/maço',
+    imagem: 'imagens_temperos/tomilho.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 42,
+    nome: 'Louro',
+    preco: 'R$3,50/pacote',
+    imagem: 'imagens_temperos/louro.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 43,
+    nome: 'Pimenta Dedo-de-Moça',
+    preco: 'R$15,00/kg',
+    imagem: 'imagens_temperos/pimenta.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 44,
+    nome: 'Gengibre',
+    preco: 'R$14,90/kg',
+    imagem: 'imagens_temperos/gengibre.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 45,
+    nome: 'Sálvia',
+    preco: 'R$4,00/maço',
+    imagem: 'imagens_temperos/salvia.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 46,
+    nome: 'Capim-Santo',
+    preco: 'R$3,50/maço',
+    imagem: 'imagens_temperos/capim.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 47,
+    nome: 'Alho-Poró',
+    preco: 'R$6,00/uni',
+    imagem: 'imagens_temperos/alho-poro.jpg',
+    categoria: 'temperos'
+  },
+  {
+    id: 48,
+    nome: 'Rúcula',
+    preco: 'R$3,50/maço',
+    imagem: 'imagens_temperos/rucula.jpg',
+    categoria: 'temperos'
+  }
+];
